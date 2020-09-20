73f2b3b2e7bee89dfd0d224bf448bfeb3fe80320
Foot-Switch <robert.rausel@gmail.com>
2020-09-20 19:30:48 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,14 @@ import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
 import { FactCardComponent } from './fact-card/fact-card.component';
 import { FactCardListComponent } from './fact-card-list/fact-card-list.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
     RouterModule.forRoot([
       { path: '', component: FactCardListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
