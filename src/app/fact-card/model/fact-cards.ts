b3306cd1ee2b7b97d03142f40e1a2a0ccc9be4f4
Foot-Switch <robert.rausel@gmail.com>
2020-09-20 19:16:04 +0200

    1 file modified

--- src/app/fact-card/model/fact-cards.ts
+++ src/app/fact-card/model/fact-cards.ts
@@ -11,7 +11,6 @@ export const factCards = [
         chapterName: "Chapter two",
         title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         numFacts: 2,
-        coverPictureUrl: "https://i.picsum.photos/id/280/200/200.jpg?hmac=jjE26x23XLyRTL5st1-nb_BAHldv1V3JoFRn4g_nN04"
     },
     {
         id: "3",
